61b615d840aa6b2fd9fac65e44ad2e04762b6b40
sginko <s.ginkota@gmail.com>
2024-03-22 15:48:02 +0100

    create constructor ang get JokeDto

--- src/main/java/pl/akademiaspecjalistowit/jokeapp/model/dto/JokeDto.java
+++ src/main/java/pl/akademiaspecjalistowit/jokeapp/model/dto/JokeDto.java
@@ -4,4 +4,19 @@ public class JokeDto {
     private String setup;
     private String delivery;
     private String category;
+
+    public JokeDto() {
+    }
+
+    public String getSetup() {
+        return setup;
+    }
+
+    public String getDelivery() {
+        return delivery;
+    }
+
+    public String getCategory() {
+        return category;
+    }
 }
