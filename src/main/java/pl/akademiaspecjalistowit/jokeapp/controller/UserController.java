deedb03c34d96d6f5d18eb0bb000481cde1be6ba
sginko <s.ginkota@gmail.com>
2024-03-22 21:25:32 +0100

    some changes

--- src/main/java/pl/akademiaspecjalistowit/jokeapp/controller/UserController.java
+++ src/main/java/pl/akademiaspecjalistowit/jokeapp/controller/UserController.java
@@ -5,6 +5,6 @@ import pl.akademiaspecjalistowit.jokeapp.model.domain.Joke;
 public class UserController {
     Joke joke = new Joke();
     public void run() {
-        joke.getContent();
+        System.out.println(joke.getContent());
     }
 }
